3fde4db234fb50eb6ffd9bed4610f27aa0184e29
ngladstone <nsg0012@auburn.edu>
2020-04-06 14:00:29 -0500

    lots of copying and pasting from other scripts later...I think it works

--- translate_orf.py
+++ translate_orf.py
@@ -1,6 +1,62 @@
 #! /usr/bin/env python3
 
+###I pulled the VAST majority of this straight from the find_orf script
+###largely relating to the use of argparse. The main individual contribution
+###is the last two lines of me combining the use of translate.py and this new
+###script.
+
 import sys
 import re
+import find_orf
+import translate
+import argparse
 
+def translate_orf():
 
+    orf_parse = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
+    # Tell the parser what command-line arguments this script can receive
+    parser.add_argument('sequence',
+            metavar = 'SEQUENCE',
+            type = str,
+            help = ('The sequence to search for an open-reading frame. '
+                    'If the path flag (\'-p\'/\'--path\') is specified, '
+                    'then this should be a path to a file containing the '
+                    'sequence to be searched.'))
+    parser.add_argument('-p', '--path',
+            action = 'store_true',
+            help = ('The sequence argument should be treated as a path to a '
+                    'containing the sequence to be searched.'))
+    parser.add_argument('-s', '--start-codons',
+            type = str,
+            nargs = '+', # one or more arguments
+            default = ['AUG'],
+            help = ('One or more possible start codons.'))
+    parser.add_argument('-x', '--stop-codons',
+            type = str,
+            nargs = '+', # one or more arguments
+            default = ['UAA', 'UAG', 'UGA'],
+            help = ('One or more possible stop codons.'))
+
+    # Parse the command-line arguments into a 'dict'-like container
+    args = parser.parse_args()
+
+    # Check to see if the path option was set to True by the caller. If so, par$
+    # the sequence from the path
+    if args.path:
+        sequence = parse_sequence_from_path(args.sequence)
+    else:
+    sequence = args.sequence
+
+    orf = find_orf.find_first_orf(sequence = sequence,
+            start_codons = args.start_codons,
+            stop_codons = args.stop_codons)
+
+    #pulled from translate.py
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T',$
+
+    translating_time = translate.translate_sequence(rna_sequence = orf, genetic_code = genetic_code
+    sys.stdout.write('{}\n'.format(translating_time))
+
+    if __name__ == '__main__':
+        main()
